37b751446e033961cb17509bb8789cb8e1de0085
misaki.tomita <misakitomita@misakinoMacBook-Pro.local>
2020-12-07 16:26:49 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -35,32 +33,43 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated method stub
         //response.getWriter().append("Served at: ").append(request.getContextPath());
 
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        //おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+      //ここから下、引っ越し
+
+       // EntityManager em = DBUtil.createEntityManager();
+       //
 
         //Messageのインスタンスを生成
-        Message m = new Message();
+        //Message m = new Message();
 
         //mの各フィールドにデータを代入
-        String title = "taro";
-        m.setTitle(title);
+        //String title = "taro";
+       // m.setTitle(title);
 
-        String content = "hello";
-        m.setContent(content);
+        //String content = "hello";
+        //m.setContent(content);
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+       // Timestamp currentTime = new Timestamp(System.currentTimeMillis());
         //現在日時を取得
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
+       // m.setCreated_at(currentTime);
+       // m.setUpdated_at(currentTime);
 
         //データベースに保存
-        em.persist(m);
-        em.getTransaction().commit();
+       // em.persist(m);
+        //em.getTransaction().commit();
 
         //自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+        //response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
-        em.close();
+       // em.close();
     }
 
 }
